6e1d97c17ed9bdd5001226f1f6c1f338bec52a23
IainMiddlehurst <iain.middlehurst@bbc.co.uk>
2021-12-02 19:47:47 +0000

    day 2 part 2 done

--- day-2/index.js
+++ day-2/index.js
@@ -3,14 +3,16 @@ var fs = require("fs");
 function calculatePosition(input) {
     let horizontal = 0;
     let depth = 0;
+    let aim = 0;
     input.map(el => {
         const split = el.split(" ");
         if(split[0] === "forward") {
             horizontal = horizontal + parseInt(split[1]);
+            depth = depth + (aim * parseInt(split[1]));
         } else if(split[0] == "down") {
-            depth = depth + parseInt(split[1]);
+            aim = aim + parseInt(split[1]);
         } else {
-            depth = depth - parseInt(split[1]);
+            aim = aim - parseInt(split[1]);
         }
     })
 
@@ -19,7 +21,6 @@ function calculatePosition(input) {
 
 function calculatePositionFromFile() {
     var input = fs.readFileSync("day-2/data.txt", "utf-8").split("\n")
-    console.log(input);
 
     return calculatePosition(input);
 }
--- tests/day-2.test.js
+++ tests/day-2.test.js
@@ -1,21 +1,21 @@
 const main = require('../day-2/index');
 
-/* part one */
+/* part one refactored for part two */
 
-test('forward 5 down 1 equals 5', () => {
-    expect(main.calculatePosition(['forward 5', 'down 1'])).toBe(5);
+test('down 1 forward 5 equals 25', () => {
+    expect(main.calculatePosition(['down 1', 'forward 5'])).toBe(25);
 });
 
-test('forward 1 down 2 equals 2', () => {
-    expect(main.calculatePosition(['forward 1', 'down 2'])).toBe(2);
+test('down 2 forward 1 equals 2', () => {
+    expect(main.calculatePosition(['down 2', 'forward 1'])).toBe(2);
 });
 
-test('forward 1 down 3 up 2 equals 1', () => {
-    expect(main.calculatePosition(['forward 1', 'down 3', 'up 2'])).toBe(1);
+test('down 3 up 2 forward 2 equals 3', () => {
+    expect(main.calculatePosition(['down 3', 'up 2', 'forward 2'])).toBe(4);
 });
 
 test('sample data outputs expected out', () => {
-    expect(main.calculatePosition(['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2'])).toBe(150);
+    expect(main.calculatePosition(['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2'])).toBe(900);
 });
 
 test('calculatePositionFromFile works', () => {
