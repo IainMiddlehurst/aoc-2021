7796be24d8349537b4e48aca44da3b411e367f82
IainMiddlehurst <iain.middlehurst@bbc.co.uk>
2021-12-06 19:47:52 +0000

    threw together day 3 part 2. Please don't judge me

--- day-3/index.js
+++ day-3/index.js
@@ -4,12 +4,49 @@ const addCurrentToPrevious = (previousValue, currentValue) => {
     return currentValue.map((el, index) => parseInt(el) + parseInt(previousValue[index]))
 }
 
+function getMostCommonBits(input) {
+    return input.map(el => el.split('')).reduce(addCurrentToPrevious).map(el => Math.round(el / input.length));
+}
+
+function flipBinaryArray(binArray) {
+    return binArray.map(el => el ? 0 : 1);
+}
+
 function calculatePowerConsumption(input) {
-    const totalOfEachDigit = input.map(el => el.split('')).reduce(addCurrentToPrevious)
-    const gamma = totalOfEachDigit.map(el => Math.round(el / input.length));
-    const epsilon = gamma.map(el => el ? 0 : 1);
+    const gamma = getMostCommonBits(input)
+    const epsilon = flipBinaryArray(gamma);
 
     return convertFromBinaryToInt(gamma.join('')) * convertFromBinaryToInt(epsilon.join(''));
+}
+
+function calculteO2AndLifeSupport(input) {
+    const mostCommonBits = getMostCommonBits(input);
+    let filteredInput = input;
+    let oxygen;
+    let lifeSupport;
+    
+    mostCommonBits.forEach((commonBit, index) => {
+        if(filteredInput.length > 1) {
+            const realMostCommonBit = getMostCommonBits(filteredInput);
+            filteredInput = filteredInput.filter(el => el[index] == realMostCommonBit[index]);
+            if(filteredInput.length === 1) {
+                oxygen = filteredInput;
+            }
+        } 
+    });
+
+    filteredInput = input;
+    flipBinaryArray(mostCommonBits).forEach((rareBit, index) => {
+        if(filteredInput.length > 1) {
+            const realRareBit = flipBinaryArray(getMostCommonBits(filteredInput));
+            filteredInput = filteredInput.filter(el => el[index] == realRareBit[index]);
+            if(filteredInput.length === 1) {
+                lifeSupport = filteredInput;
+            }
+        } 
+    });
+    
+    return convertFromBinaryToInt(oxygen[0]) * convertFromBinaryToInt(lifeSupport[0]);
 }
 
 function convertFromBinaryToInt(string) {
@@ -23,7 +60,15 @@ function calculatePowerConsumptionFromFile() {
     return calculatePowerConsumption(input);
 }
 
+function calculteO2AndLifeSupportFromFile() {
+    var input = fs.readFileSync("day-3/data.txt", "utf-8").split("\n")
+
+    return calculteO2AndLifeSupport(input);
+}
+
 module.exports = {
     calculatePowerConsumption,
-    calculatePowerConsumptionFromFile
+    calculatePowerConsumptionFromFile,
+    calculteO2AndLifeSupport,
+    calculteO2AndLifeSupportFromFile
 }
--- tests/day-3.test.js
+++ tests/day-3.test.js
@@ -2,16 +2,28 @@ const main = require('../day-3/index');
 
 /* part one */
 
-test('01,11,01 outputs 2', () => {
+test('calculatePowerConsumption should return 2 with input 01,11,01', () => {
     expect(main.calculatePowerConsumption(['01', '11', '01'])).toBe(2);
 });
 
-test('sample data should output 198', () => {
+test('calculatePowerConsumption should return 198 with sample data', () => {
     expect(main.calculatePowerConsumption(
         ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010'])
     ).toBe(198);
 });
 
-test('should work', () => {
-    console.log(main.calculatePowerConsumptionFromFile());
+test('get solution from file', () => {
+    // console.log(main.calculatePowerConsumptionFromFile());
+});
+
+/* part two */
+
+test('calculteO2AndLifeSupport should return 230 with sample data', () => {
+    expect(main.calculteO2AndLifeSupport(
+        ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010'])
+    ).toBe(230);
+});
+
+test('calculteO2AndLifeSupport solution from file', () => {
+    // console.log(main.calculteO2AndLifeSupportFromFile());
 });
